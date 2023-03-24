9286b12c2a1ecf9e8b1b2b82da70ec075a386793
Paul Gierz <pgierz@mac.de>
2023-03-24 16:41:48 +0100

    first steps towards a Python package template Paul would feel happy reading

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is a Python-based FESOM project to help you work with FESOM, AWI's Ocean Model.
+
+This file can be used to import other libraries.
+
+If you get stuck, feel free to contact:
+    * Paul Gierz (Python help, Computing help)
+    * Miguel Andres-Martinez (Python help, Computing help)
+    * Dmitry Sidorenko (Core FESOM Developer)
+    * Nikolay Kuldanov (Core FESOM Developer, Python help)
+    * Patrick Scholz (Core FESOM Developer, FORTRAN help)
+
+Happy Coding!
+Your HPC and FESOM Teams
+"""
+# Do not forget to replace the doc-string with something related to your project ;-)
---
+++ src/cli.py
@@ -0,0 +1,9 @@
+#!/bin/usr/env python3
+
+
+def main():
+    print("Hello from my favorite Python based project to work with FESOM!")
+
+
+if __name__ == "__main__":
+    main()
